e3bb8fb100b96194b186bee87c545e7f932947ce
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-09 16:07:39 -0300

    basic css style for table header

--- src/components/CoinTable/CoinTable.js
+++ src/components/CoinTable/CoinTable.js
@@ -4,10 +4,25 @@ import './CoinTable.css';
 export const CoinTable = () => {
   return (
     <div className='coin-list'>
+      <table className='content-list'>
+        <thead>
+          <tr>
+            <th className='coin-name'>Coin</th>
+            <th className='coin-price left-side'>Price</th>
+            <th>1h</th>
+            <th>24h</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>teste</td>
+            <td className='left-side'>teste</td>
+            <td>teste</td>
+            <td>teste</td>
+          </tr>
+        </tbody>
+      </table>
       {/* Fazer titulos e cada item com fundo gray */}
-      <div>
-        <h1>teste</h1>
-      </div>
     </div>
   );
 };
