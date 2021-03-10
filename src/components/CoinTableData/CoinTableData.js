aa7e0252e6e0bc1b7d9597c79980eb0dc506f2fc
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-10 09:02:40 -0300

    current price converted to show comma

--- src/components/CoinTableData/CoinTableData.js
+++ src/components/CoinTableData/CoinTableData.js
@@ -8,7 +8,9 @@ export const CoinTableData = ({ item }) => {
           <img src={item.image} alt='' />
           {item.symbol.toUpperCase()}
         </td>
-        <td className='left-sidee'>$53,790.40</td>
+        <td className='left-sidee'>
+          ${item.current_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </td>
         <td className='text-danger'>-0.2%</td>
         <td className='text-green'>6.1%</td>
         <td className='text-green'>8.1%</td>
